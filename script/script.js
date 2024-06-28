14292f2512fde893dcbd7695608e508ca4f7152c
YehezkielG <112444049+YehezkielG@users.noreply.github.com>
2024-06-28 21:33:04 +0700

    show trending movie

---
+++ script/script.js
@@ -0,0 +1,25 @@
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMWQyZDcxM2U5MWM3MzJjYWMzNGQ0ZTdjNGRhOWUzZCIsInN1YiI6IjY2NjliMzg4NDc0MmYyZmM1NjViM2Y5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y6ALOgzkxh_hL3sBPJAGSa1LfpwYfKbDKdnU7saFHKQ'
+  }
+};
+
+fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
+  .then(response => response.json())
+  .then((response) => {
+    // console.log(response);
+    const result = response.results;
+    console.log(result);
+    result.forEach((v,i)=>{
+      const div = document.createElement("div");
+      div.className = "text-black inline-block bg-white p-2 rounded-xl m-2";
+      div.style.Width = "250px";
+      div.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/w300${v.poster_path}" alt="" style = "min-width: 250px;">
+      <p class="">${v.original_title}</p>`
+      document.querySelector(".trendingMovie").appendChild(div);
+    })
+  })
+  .catch(err => console.error(err));
