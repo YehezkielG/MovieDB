51953607155ca91c6c853dc3ef6a040efa64724a
YehezkielG <112444049+YehezkielG@users.noreply.github.com>
2024-06-29 22:40:01 +0700

    New design, add Footer

--- script/script.js
+++ script/script.js
@@ -14,10 +14,10 @@ fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
     console.log(result);
     result.forEach((v,i)=>{
       const div = document.createElement("div");
-      div.className = "text-black inline-block bg-white p-2 rounded-xl m-2";
-      div.style.Width = "250px";
+      div.className = "text-black inline-block p-2 rounded-xl m-2 shadow-lg border-2";
+      div.style.Width = "200px";
       div.innerHTML = `
-      <img src="https://image.tmdb.org/t/p/w300${v.poster_path}" alt="" style = "min-width: 250px;">
+      <img src="https://image.tmdb.org/t/p/w300${v.poster_path}" alt="" style = "min-width: 200px;" style='m-4'>
       <p class="">${v.original_title}</p>`
       document.querySelector(".trendingMovie").appendChild(div);
     })
