2a85b96f4c7a3f61db9ebba684029864d1fa23af
YehezkielG <112444049+YehezkielG@users.noreply.github.com>
2024-07-04 23:27:13 +0700

    add icon,request trending tv

--- script/script.js
+++ script/script.js
@@ -8,12 +8,10 @@ const options = {
 
 let url = {
   Trending : fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US',options).then(response => response.json()),
-  Popular : fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options).then(response => response.json())
+  TrendingTV : fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US',options).then(response => response.json())
 }
 
-
-
-const date = ["Jan","Feb","Mar","Apr","May","June","Jul","Aug","Sep","Oct","Nov","Dec"];
+const monthFormat = ["Jan","Feb","Mar","Apr","May","June","Jul","Aug","Sep","Oct","Nov","Dec"];
 
 async function request(){
   let ResponseView = Object.keys(url);
@@ -23,14 +21,15 @@ async function request(){
   console.log(result);
   result.forEach((value,index)=>{
     value.results.forEach((v)=>{
-    const year = v.release_date.substr(0,4);
-    const month = date[parseInt(v.release_date.substr(6,7))-1];
+    const date = Object.values(v)[11]; 
+    const year = date.substr(0,4);
+    const month = monthFormat[parseInt(date.substr(6,7))-1];
     const div = document.createElement("div");
-    div.className = "text-black inline-block p-2 rounded-xl m-2 shadow-lg border-2";
+    div.className = "text-black inline-block p-2 rounded-xl m-2 shadow-lg cursor-pointer";
     div.style.Width = "200px";
     div.innerHTML = `
     <img src="https://image.tmdb.org/t/p/w300${v.poster_path}" alt="" style = "min-width: 200px;" style='m-4'>
-    <p class="truncate font-bold" style="width:200px">${v.original_title}</p>
+    <p class="truncate font-bold" style="width:200px">${Object.values(v)[4]}</p>
     <small class="text-sm text-gray-500"> ${month} ${year}</small>`
     document.querySelector(`#${ResponseView[index]}`).appendChild(div);
     })
