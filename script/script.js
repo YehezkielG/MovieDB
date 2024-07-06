cac499e42d8be55f511cfc21cddde3432bbe825c
YehezkielG <112444049+YehezkielG@users.noreply.github.com>
2024-07-06 23:25:22 +0700

    now users can see day or this week trending

--- script/script.js
+++ script/script.js
@@ -7,13 +7,13 @@ const options = {
 };
 
 let url = {
-  Trending : fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US',options).then(response => response.json()),
-  TrendingTV : fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US',options).then(response => response.json())
+  Trendingmovie : fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US',options).then(response => response.json()),
+  Trendingtv : fetch('https://api.themoviedb.org/3/trending/tv/day?language=en-US',options).then(response => response.json())
 }
 
 const monthFormat = ["Jan","Feb","Mar","Apr","May","June","Jul","Aug","Sep","Oct","Nov","Dec"];
 
-async function request(){
+async function request(url){
   let ResponseView = Object.keys(url);
   url = Object.values(url);
   let request = await Promise.all(url);
@@ -25,7 +25,7 @@ async function request(){
     const year = date.substr(0,4);
     const month = monthFormat[parseInt(date.substr(6,7))-1];
     const div = document.createElement("div");
-    div.className = "text-black inline-block p-2 rounded-xl m-2 shadow-xl cursor-pointer";
+    div.className = "text-black inline-block p-2 rounded-xl m-2 shadow-xl border-2 border-gray-50 cursor-pointer";
     div.style.Width = "200px";
     div.innerHTML = `
     <img src="https://image.tmdb.org/t/p/w300${v.poster_path}" alt="" style = "min-width: 200px;" style='m-4'>
@@ -40,6 +40,15 @@ async function request(){
   })
 }
 
+function setDayTrending(element){
+  const day = element.innerHTML == "hari ini" ? "day" : "week";
+  const url = {};
+  url["Trending"+element.value] = fetch(`https://api.themoviedb.org/3/trending/${element.value}/${day}?language=en-US`,options).then(response => response.json());
+  document.getElementById("Trending"+element.value).innerHTML = "";
+  console.log(url);
+  request(url);
+}
+
 document.body.onload = ()=>{
-  request();
+  request(url);
 }
